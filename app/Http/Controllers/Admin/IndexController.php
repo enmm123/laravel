89fb10288959bd5c6b5ecfbecfd690b88f755675
enmm123 <154611187@qq.com>
2020-02-27 16:54:24 +0800

    优化后台页面

--- app/Http/Controllers/Admin/IndexController.php
+++ app/Http/Controllers/Admin/IndexController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Model\Article;
+use App\Model\Comment;
+use App\Model\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -13,7 +16,23 @@ class IndexController extends Controller
     }
     //后台首页主体
     public function welcome(){
-        return view('admin.welcome');
+        $time = date('Y-m-d H:i:s',time());
+        $art_num = Article::count();
+        $user_num = User::count();
+        $com_num = Comment::count();
+        $info = array(
+            'time'=>$time,
+            'art_num'=>$art_num,
+            'user_num'=>$user_num,
+            'com_num'=>$com_num,
+            'php_uname'=>php_uname(),
+            'php_sapi'=>php_sapi_name(),
+            'php_version'=> PHP_VERSION,
+            'server_ip'=>GetHostByName($_SERVER['SERVER_NAME']),
+            'client_ip'=>$_SERVER['REMOTE_ADDR']
+        );
+//        var_dump($info);
+        return view('admin.welcome',compact('info'));
     }
     //后台退出
     public function logout(){
